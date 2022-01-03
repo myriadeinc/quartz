c8b2e42a940b1105135b5219049f489c362545aa
Basile Fengos <basilefengos@Basiles-MacBook-Air.local>
2022-01-03 16:17:01 -0500

    Boilerplate init

---
+++ src/main/main.ts
@@ -0,0 +1,89 @@
+import { app, BrowserWindow, shell } from "electron";
+import { autoUpdater } from "electron-updater";
+import MenuBuilder from "./menu";
+import log from "electron-log";
+import { resolveHtmlPath } from "./util";
+
+export default class AppUpdater {
+  constructor() {
+    log.transports.file.level = "info";
+    autoUpdater.logger = log;
+    autoUpdater.checkForUpdatesAndNotify();
+  }
+}
+
+let mainWindow: BrowserWindow | null = null;
+
+if (process.env.NODE_ENV === "production") {
+  const sourceMapSupport = require("source-map-support");
+  sourceMapSupport.install();
+}
+
+const isDevelopment =
+  process.env.NODE_ENV === "development" || process.env.DEBUG_PROD === "true";
+
+if (isDevelopment) {
+  require("electron-debug")();
+}
+
+const installExtensions = async () => {
+  const installer = require("electron-devtools-installer");
+  const forceDownload = !!process.env.UPGRADE_EXTENSIONS;
+  const extensions = ["REACT_DEVELOPER_TOOLS"];
+
+  return installer
+    .default(
+      extensions.map((name) => installer[name]),
+      forceDownload
+    )
+    .catch(console.log);
+};
+
+const createWindow = async () => {
+  if (isDevelopment) {
+    await installExtensions();
+  }
+
+  mainWindow = new BrowserWindow({
+    width: 1024,
+    height: 768,
+  });
+
+  mainWindow.loadURL(resolveHtmlPath("index.html"));
+
+  mainWindow.on("ready-to-show", () => {
+    if (!mainWindow) {
+      throw new Error('"mainWindow" is not defined');
+    }
+    if (process.env.START_MINIMIZED) {
+      mainWindow.minimize();
+    } else {
+      mainWindow.show();
+    }
+  });
+
+  mainWindow.on("closed", () => {
+    mainWindow = null;
+  });
+
+  const menuBuilder = new MenuBuilder(mainWindow);
+  menuBuilder.buildMenu();
+
+  // Open urls in the user's browser
+  mainWindow.webContents.on("new-window", (event, url) => {
+    event.preventDefault();
+    shell.openExternal(url);
+  });
+
+  // Remove this if your app does not use auto updates
+  // eslint-disable-next-line
+  new AppUpdater();
+};
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
